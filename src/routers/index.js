f1f5361ff24514641352698ee9a8c8866063976e
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2026-03-01 10:59:48 +0100

    added vendor dashboard

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,31 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req,res) => {
-	try{
-		res.status(200).render('index',{
-			title:"welcome to Vendboost",
-		})
-	}catch(err){
-		return res.status(500).json({
-	      message: "Server error: something went wrong, please try again later",
-	      error: err,
-	     });
-	}
-})
-
-router.get('/signup', (req, res) => {
-    res.render('signup',{
-    	title:"Create Account – VendBoost"
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).render("index", {
+      title: "welcome to Vendboost",
     });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Server error: something went wrong, please try again later",
+      error: err,
+    });
+  }
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup", {
+    title: "Create Account – VendBoost",
+  });
 });
 
 // Optional: disable POST signups until launch
-router.get('/signin', (req, res) => {
-     res.render('signin',{
-    	title:"Sign In – VendBoost"
-    });
+router.get("/signin", (req, res) => {
+  res.render("signin", {
+    title: "Sign In – VendBoost",
+  });
 });
 
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", {
+    title: "Dashboard – VendBoost",
+  });
+});
 
 module.exports = router;
